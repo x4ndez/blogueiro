ed2997eab877d2f8fd1aa041dab82f6ff0f11551
x.ande <ananfro@live.com>
2023-09-09 23:25:14 +1000

    handlebars setup with routes

--- controllers/indexRoutes.js
+++ controllers/indexRoutes.js
@@ -2,9 +2,27 @@ const router = require("express").Router();
 
 // PATH: localhost/
 
-router.get("/*", (req, res) => {
+router.get("/", (req, res) => {
 
     res.render("index");
+
+});
+
+router.get("/login", (req, res) => {
+
+    res.render("login");
+
+});
+
+router.get("/login/new-account", (req, res) => {
+
+    res.render("new-account");
+
+});
+
+router.get("*", (req, res) => {
+
+    res.render("pnf");
 
 });
 
