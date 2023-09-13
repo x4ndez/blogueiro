2b1d3e80bf8b11e47bb13a2aba9d4e44bf459f47
x.ande <ananfro@live.com>
2023-09-14 08:26:43 +1000

    blogposts on home and comments now display formatted timestamps

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,5 +1,17 @@
 const submit = document.querySelector("#submit");
 const content = document.querySelector("#content");
+const timestamp = document.querySelectorAll(".timestamp");
+
+for (let item of timestamp) {
+
+    let commentTimestampRaw = item.getAttribute("data-raw");
+    // console.log(commentTimestampRaw);
+    let commentTimestampHour = dayjs(commentTimestampRaw).format("hh:mm a");
+    let commentTimestampDate = dayjs(commentTimestampRaw).format("DD/MM/YY");
+    console.log(commentTimestampHour, commentTimestampDate);
+    item.innerText = `${commentTimestampHour} at ${commentTimestampDate}`;
+
+}
 
 const postId = submit.getAttribute("data-id");
 const commentUser = submit.getAttribute("data-user");
